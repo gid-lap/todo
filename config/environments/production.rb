fec6f290fea68c208e7f2f4308ee58ec95a7d0f1
gid-lap <84563701+gid-lap@users.noreply.github.com>
2021-07-26 18:24:19 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = { :authentication => :plain, :address => "smtp.sendgrid.net", :port => 587, :domain => "engineyard.com", :user_name => "glapshun@engineyard.com"), :api_key => ENV['SENDGRID_API_KEY'], :authentication => 'plain', :enable_starttls_auto => true }
+  config.action_mailer.smtp_settings = { :authentication => :plain, :address => "smtp.sendgrid.net", :port => 587, :domain => "engineyard.com", :user_name => "glapshun@engineyard.com", :api_key => ENV['SENDGRID_API_KEY'], :authentication => 'plain', :enable_starttls_auto => true }
 end
